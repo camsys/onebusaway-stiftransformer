6684f7d4aef5e3f4a5c486de199b948ef61bd525
caylasavitzky <caysavitzky@gmail.com>
2020-01-28 10:31:13 -0500

    Updates in reading stif files into boroughs and holiday vs nonholiday

--- src/test/java/org/onebusaway/onebusaway_stif_transformer/StifBoroughCategorizorTest.java
+++ src/test/java/org/onebusaway/onebusaway_stif_transformer/StifBoroughCategorizorTest.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.onebusaway.onebusaway_stif_transformer.StifSupport;
+import org.onebusaway.onebusaway_stif_transformer.impl.StifLoaderImpl;
 import org.onebusaway.onebusaway_stif_transformer.transformer.StifTransformFactory;
 import org.onebusaway.onebusaway_stif_transformer.transformer.StifTransformer;
 import org.onebusaway.onebusaway_stif_transformer.transformer.TransformContext;
@@ -18,8 +21,17 @@ import org.onebusaway.onebusaway_stif_transformer.transformer.TransformSpecifica
 public class StifBoroughCategorizorTest {
     @Test
     public void testStifBoroughCategorizor(){
+        StifLoaderImpl stifLoader = new StifLoaderImpl();
+        File stifsPath = new File("/Users/caylasavitzky/src/mtaoba/onebusaway-stiftransformer/src/test/resources/stifs_mtabc");
+        List<File> stifFiles = new ArrayList<>();
+        stifFiles.add(stifsPath);
+        HashMap<String,StifSupport> stifsMap = new HashMap<String,StifSupport>();
+        stifLoader.load(stifFiles, stifsMap);
+
         StifBoroughCategorizor stifBoroughCategorizor = new StifBoroughCategorizor();
-        //stifBoroughCategorizor.initialize();
+        for (Map.Entry<String, StifSupport> entry : stifsMap.entrySet()) {
+            stifBoroughCategorizor.initialize(entry.getValue());
+        }
 
     }
 }
