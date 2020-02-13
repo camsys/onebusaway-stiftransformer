db3e763879ef3b5ca0236f07c1e167e0e0704985
caylasavitzky <caysavitzky@gmail.com>
2020-02-13 12:07:52 -0500

    A number of small updates to the tests, and updates to the poms, and man page

--- onebusaway-stif-transformer-impl/src/test/java/onebusaway_stif_transformer_impl/TestStifTransformerSuite.java
+++ onebusaway-stif-transformer-impl/src/test/java/onebusaway_stif_transformer_impl/TestStifTransformerSuite.java
@@ -1,11 +1,13 @@
 package onebusaway_stif_transformer_impl;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.onebusaway.onebusaway_stif_transformer_impl.StifTransformerSuite;
 
 public class TestStifTransformerSuite {
+    @Ignore
     @Test
-    public void testLoadAndTransformFromStringAndPrint() {
+    public void testLoadAndTransformFromSiteAndPrint() {
 
         String[] inputPaths = new String[1];
         inputPaths[0] = "/Users/caylasavitzky/src/mtaoba/onebusaway-stiftransformer/onebusaway-stif-transformer-impl/src/test/resources/stifs_mtabc";
@@ -20,12 +22,30 @@ public class TestStifTransformerSuite {
         transformerSuite.run();
     }
 
+    @Ignore
     @Test
     public void testLoadAndTransformFromFileAndPrint() {
 
         String[] inputPaths = new String[1];
         inputPaths[0] = "/Users/caylasavitzky/src/mtaoba/onebusaway-stiftransformer/onebusaway-stif-transformer-impl/src/test/resources/stifs_mtabc";
         String transform = "/Users/caylasavitzky/src/mtaoba/onebusaway-stiftransformer/onebusaway-stif-transformer-impl/src/test/resources/loadTransformByFileTest.json";
+        String outputPath = "/Users/caylasavitzky/Downloads/Stif-transformer" + System.currentTimeMillis();
+
+        StifTransformerSuite transformerSuite = new StifTransformerSuite();
+        transformerSuite.setInputPaths(inputPaths);
+        transformerSuite.setTranform(transform);
+        transformerSuite.setOutputPath(outputPath);
+        transformerSuite.run();
+    }
+
+    @Ignore
+    @Test
+    public void testLoadAndTransformFromStringAndPrint() {
+
+        String[] inputPaths = new String[1];
+        inputPaths[0] = "/Users/caylasavitzky/src/mtaoba/onebusaway-stiftransformer/onebusaway-stif-transformer-impl/src/test/resources/stifs_mtabc";
+        //inputPaths[0] = "/var/folders/2t/k5nkxf3s49153ccm9h7wqmx40000gn/T/tmp1580408549199/stif/stif/non_holiday/stif.m_0034sb.212123.sat";
+        String transform = "{\"op\":\"update\",\"match\":{\"class\":\"EventRecord\",\"distanceFromStartOfTrip\":\"0\"},\"update\":{\"boardingAlightingFlag\":\"m\"}}";
         String outputPath = "/Users/caylasavitzky/Downloads/Stif-transformer" + System.currentTimeMillis();
 
         StifTransformerSuite transformerSuite = new StifTransformerSuite();
